dc228aae8f6d31fc91723839c3dfa113b9460d7e
Michael Wood <michaelwood33311@icloud.com>
2023-05-14 17:41:32 -0400

    added basic chakra styling and heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeCounterValue } from './store/sliceCounter';
-import { Button, Container, Text } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Text } from '@chakra-ui/react';
 
 function App() {
   const dispatch = useDispatch();
@@ -9,12 +9,13 @@ function App() {
 
   const activeTree = useSelector(state => state.activeTree);
   return (
-    <Container>
-      <div className="App" style={{display:'flex', alignItems:'center'}}>
-        <Button variant="primary" onClick={() => dispatch(changeCounterValue({amount: 1}))}>+</Button>
-        <Text fontSize="2rem" width='2rem' textAlign={'center'}>{value}</Text>
-        <Button variant="primary" onClick={() => dispatch(changeCounterValue({amount: -1}))}>-</Button>
-      </div>
+    <Container textAlign={'center'}>
+        <Heading>Counter</Heading>
+        <Box display='flex' justifyContent={'center'} alignItems='center'>
+          <Button variant="primary" onClick={() => dispatch(changeCounterValue({amount: 1}))}>+</Button>
+          <Text fontSize="2rem" width='2rem' textAlign={'center'}>{value}</Text>
+          <Button variant="primary" onClick={() => dispatch(changeCounterValue({amount: -1}))}>-</Button>
+        </Box>
     </Container>
   );
 }
