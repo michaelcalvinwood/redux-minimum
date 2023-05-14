b468f86a89dbab7402b7379b3af41840f68d5c48
Michael Wood <michaelwood33311@icloud.com>
2023-05-14 18:31:30 -0400

    added integrating redux with socket.io util library

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,13 @@ let socket = null;
 export const setupTheSocket = (socketio, url, store) => {
     if (socket) return;
     socket = socketio(url);
-
+    console.log('dispatch')
+    store.dispatch({
+        type: 'counter/changeCounterValue',
+        payload: {
+            amount: 20
+        }
+    })
 }
 
 export const emit = (event, ...args) => socket.emit(event, ...args);
